c76650243e2f5a91f991b2590b995ef624802d14
Nox1do <noogux@gmail.com>
2022-10-04 03:45:11 +0000

    fixing rel

--- src/starwars.py
+++ src/starwars.py
@@ -8,10 +8,9 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'users'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(250), nullable=False, unique=True)
     password = Column(Integer, nullable=False, unique=True)
@@ -20,37 +19,38 @@ class User(Base):
 
 class Character(Base):
     __tablename__ = 'characters'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    birth_year = Column(Integer, nullable=True)
-    gender = Column(String(250), nullable=True)
-    character_id = Column(Integer, ForeignKey('users.id'))
-    users = relationship(User)
-
+    birth_year = Column(Integer, nullable=False)
+    gender = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     class Planets(Base):
         __tablename__ = 'Planets'
-        id =  Column(Integer, primary_key=True)
-        name = Column(String(250), nullable=True)
-        population = Column(Integer, nullable=True)
-        climate = Column(String(250), nullable=True)
-        terrain = Column(String(250), nullable=True)
-        planet_id = Column(Integer, ForeignKey('users.id'))
-        users = relationship(User)
-     
-    class Favorites(Base):
-       __tablename__ = 'Favorites'
-       id = Column(Integer, primary_key=True)
-       character_name = Column(String(250), nullable=True)
-       gplanet_name = Column(String(250), nullable=True)
-    
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250), nullable=False)
+        population = Column(Integer, nullable=False)
+        climate = Column(String(250), nullable=False)
+        terrain = Column(String(250), nullable=False)
+        user_id = Column(Integer, ForeignKey('user.id'))
+        user = relationship(User)
 
+    class Favorites_Characters(Base):
+        __tablename__ = 'Favorites-Characters'
+        id = Column(Integer, primary_key=True)
+        user_id = Column(Integer, ForeignKey('user.id'))
+        character_id = Column(Integer,ForeignKey('planets.id'))
 
+    class Favorites_Planets(Base):
+        __tablename__ = 'Favorites-Planets'
+        id = Column(Integer, primary_key=True)
+        user_id = Column(Integer, ForeignKey('user.id'))
+        planets_id = Column(Integer,ForeignKey('planets.id'))
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
+
+# Draw from SQLAlchemy base
 render_er(Base, 'starwars.png')
